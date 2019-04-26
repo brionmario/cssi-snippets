5c61d288920f8ce0ee7e4e18153cd08c9ff12e09
Brion Mario <brion.2015283@iit.ac.lk>
2019-04-26 21:00:39 +0530

    Create snippets.py

---
+++ snippets.py
@@ -0,0 +1,6 @@
+"""Snippet 1"""
+
+from cssi.core import CSSI
+
+# Create an instance of the CSSI Library
+cssi = CSSI(shape_predictor="classifiers/shape_predictor_68_face_landmarks.dat", debug=False, config_file="cssi.rc")
