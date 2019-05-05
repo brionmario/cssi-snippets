2fa31f19e07a68e1220062780d94f2860f5b7089
Brion Mario <brion.2015283@iit.ac.lk>
2019-05-06 02:01:12 +0530

    Update snippets.py

--- snippets.py
+++ snippets.py
@@ -4,9 +4,9 @@ from cssi.core import CSSI
 
 # Create an instance of the CSSI Library
 cssi = CSSI(
-    shape_predictor="classifiers/shape_predictor_68_face_landmarks.dat",
+    shape_predictor="classifiers/hmd_face_landmarks.dat",
     debug=False,
-    config_file="cssi.rc"
+    config_file="config.cssi"
 )
 
 
