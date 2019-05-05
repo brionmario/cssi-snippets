b4003f3cb379180c37f7a89e68071fa2238cd357
Brion Mario <brion.2015283@iit.ac.lk>
2019-05-06 01:58:06 +0530

    Update snippets.py

--- snippets.py
+++ snippets.py
@@ -13,12 +13,7 @@ class CSSI(object):
     """The main access point for the CSSI library"""
 
     def __init__(self, shape_predictor, debug=False, config_file=None):
-        """ Initializes all the core modules in the CSSI Library.
-
-        :param shape_predictor: Path to the landmark detector.
-        :param debug: Boolean indicating if debug mode should be activated or not.
-        :param config_file: A file containing all the configurations for CSSI.
-        """
+        """Initializes all the core modules in the CSSI Library."""
         # If no config file name is passed in, defaults to `config.cssi`
         if config_file is None:
             self.config_file = "config.cssi"
@@ -33,8 +28,6 @@ class CSSI(object):
         self.sentiment = Sentiment(config=self.config, debug=self.debug)
         # Initializing the questionnaire module.
         self.questionnaire = SSQ(config=self.config, debug=self.debug)
-        # Initializing the plugins specified in the configuration file
-        self.plugins = Plugins.init_plugins(modules=self.config.plugins, config=self.config, debug=self.debug)
         logger.debug("CSSI library initialized......")
 
        
